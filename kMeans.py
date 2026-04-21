939b5832519511b4f122e4f76b7d779c82a6b085
Olivia Teng <xad2vu@virginia.edu>
2026-04-21 22:07:49 +0000

    kMeans

--- kMeans.py
+++ kMeans.py
@@ -16,8 +16,27 @@ car = pd.read_csv('car_price_dataset.csv')
 car.head()
 # %%
 # Drop unnecessary columns
-car = car.drop(['Color', 'Interior', 'DriveType','Options', 'City', 'RegistrationStatus', 'FuelType'], axis=1)
+# Keep CarAge, Condition, Mileage, Horsepower, Torque, Transmission, BodyType, Interior, AccidentHistory 
+car_df = car.drop(['Color', 'Year','Interior', 'EngineSize(L)', 'Doors', 'Insurance', 'DriveType','Options', 'Seats','City', 'RegistrationStatus', 'FuelType'], axis=1)
 
 # %%
-car.head()
+car_df.head()
+
+# %%
+# One hot encode categorical variables
+car_df = pd.get_dummies(car_df, columns=['Condition', 'BodyType', 'AccidentHistory'], drop_first=False, dtype=int)
+# %%
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(car)
+# %%
+# kMeans with guess for k=3
+kmeans = KMeans(n_clusters=3, random_state=42)
+# Fit model to training data
+kmeans.fit(scaled_data)
+# Predictions
+clusters = kmeans.predict(scaled_data)
+# Show clusters
+car_df["Cluster"] = clusters
+
+car_df.head()
 # %%
